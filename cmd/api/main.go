a345da594baaf12fee3357abde91b6ad79747aee
ravachol nildock <ravacholnildock@devnull.mail>
2023-06-29 12:01:05 +0200

    feat(api): add /xtz/delegations handler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"delegationz/pkg/db"
 	"delegationz/pkg/services/api"
 	"delegationz/pkg/services/tzkt"
 	"log"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
+
+var dbURL = "postgres://postgres:supersecret@localhost:5432/dev"
 
 func main() {
 	srv := echo.New()
+	srv.Use(middleware.Logger())
 	tzktClient := tzkt.NewTzktClient()
-	srv.GET("/xtz/delegations", api.DelegationsHandler(tzktClient))
+	dbclient := db.Get(dbURL)
+	srv.GET("/v0/xtz/delegations", api.QuickDelegationsHandler(tzktClient))
+	srv.GET("/xtz/delegations", api.DelegationsHandler(dbclient))
 	log.Fatal(srv.Start(":8080"))
 }
