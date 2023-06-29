ae8514c95e8f8e342ca24afecb6e40e811c950ce
ravachol nildock <ravacholnildock@devnull.mail>
2023-06-29 09:21:59 +0200

    feat(importer): Batch import goroutine loop wip

--- cmd/importer/main.go
+++ cmd/importer/main.go
@@ -2,84 +2,104 @@ package main
 
 import (
 	"context"
-	"database/sql"
-	"delegationz/pkg/repository"
+	"delegationz/pkg/db"
 	"delegationz/pkg/services/tzkt"
-	"fmt"
 	"log"
+	"sync"
 	"time"
-
-	"github.com/jackc/pgx/v4/pgxpool"
-	_ "github.com/jackc/pgx/v4/stdlib"
-	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
+var dbURL = "postgres://postgres:supersecret@localhost:5432/dev"
+
 func main() {
-	dbURL := "postgres://postgres:supersecret@localhost:5432/dev"
-	// Configure the PostgreSQL connection
-	connConfig, err := pgxpool.ParseConfig(dbURL)
-	if err != nil {
-		log.Fatal("Error parsing connection config:", err)
+	// Create a WaitGroup to wait for all goroutines to finish
+	var wg sync.WaitGroup
+
+	// Load tzkt api client
+	cli := tzkt.NewTzktClient()
+
+	// Create a channel to communicate delegation objects between goroutines
+	delegationChannel := make(chan *tzkt.DelegationItems)
+
+	// Set the number of goroutines to use
+	numGoroutines := 5
+
+	// Launch goroutines to fetch and save delegations
+	for i := 0; i < numGoroutines; i++ {
+		wg.Add(1)
+		go saveDelegations(delegationChannel, &wg)
 	}
 
-	// Create the database connection
-	db, err := sql.Open("pgx", connConfig.ConnString())
-	if err != nil {
-		log.Fatal("Error opening database connection:", err)
-	}
-	defer db.Close()
-	// Query delegations using SQLBoiler
-	ctx := context.Background()
-	delegations, err := repository.Delegations(qm.Limit(10)).All(ctx, db)
-	if err != nil {
-		log.Fatal("Error querying delegations:", err)
-	}
-
-	log.Printf("[DEBUG] %d delegations in database", len(delegations))
-
-	// Process the retrieved delegation data
-	for _, delegation := range delegations {
-		fmt.Println("Delegation ID:", delegation.ID)
-		fmt.Println("Timestamp:", delegation.Timestamp)
-		fmt.Println("Amount:", delegation.Amount)
-		fmt.Println("Delegator:", delegation.Delegator)
-		fmt.Println("Block:", delegation.Block)
-		fmt.Println("---------------------------")
-	}
-	tzktClient := tzkt.NewTzktClient()
-	firstValDate := "2018-06-30T19:30:27Z"
-
-	ts, _ := time.Parse(time.RFC3339, firstValDate)
-	te := ts.AddDate(0, 0, 3)
-	flters := &tzkt.Filters{
-		TimestampGe: ts.Format(time.RFC3339),
-		TimestampLt: te.Format(time.RFC3339),
-		TypeEq:      "applied",
-	}
-
-	var val *tzkt.DelegationItems
-	lmts := tzkt.Pagination{
+	// Fetch the paginated items concurrently
+	pageSize := 600
+	pagination := tzkt.Pagination{
+		Limit:  pageSize,
 		Offset: 0,
-		Limit:  600,
 	}
-	for val == nil || (val != nil && val.HasMore) {
-		rr, err := tzktClient.Delegations(flters, &lmts)
+	flters := tzkt.Filters{
+		TypeEq: "applied",
+	}
+	for i := 0; i < numGoroutines; i++ {
+		resp, err := cli.Delegations(&flters, &pagination)
 		if err != nil {
-			log.Fatal(err)
+			log.Printf("[ERROR] error making API request:%v\n", err)
+			break
 		}
 
-		// for _, el := range val.Items {
-		// 	log.Printf("Address %v delegated %v Tzs to %s @ %+v\n", el.Sender.Address, el.Amount, el.NewDelegate.Address, el.Timestamp.Format(time.RFC3339))
-		// }
-		if val == nil {
-			val = rr
-		} else {
-			val.Items = append(val.Items, rr.Items...)
-			val.HasMore = rr.HasMore
+		// Send items to the channel for goroutines to process and save
+		delegationChannel <- resp
+
+		// Check if there are more pages
+		if !resp.HasMore {
+			break
 		}
-		lmts.Offset++
-		time.Sleep(1 * time.Second)
-		log.Printf("[DEBUG] %v delegations from API (more: %v)", len(val.Items), rr.HasMore)
+
+		pagination.Offset++
 	}
-	log.Printf("[DEBUG] %v delegations from API (filters: %+v)", len(val.Items), flters)
+	// Close the delegation channel to indicate that no more delegations will be sent
+	close(delegationChannel)
+
+	// Wait for all goroutines to finish
+	wg.Wait()
+
+}
+
+func saveDelegations(delegationChannel <-chan *tzkt.DelegationItems, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for delegation := range delegationChannel {
+		// Perform the logic to save the delegation to the database or perform any other operations
+		// For simplicity, we will just print the delegation here
+		ctx := context.Background()
+		log.Printf("[DEBUG] Will save %v delegation\n", len(delegation.Items))
+		if len(delegation.Items) == 0 {
+			continue
+		}
+
+		ids := make([]int64, len(delegation.Items))
+		timestamps := make([]time.Time, len(delegation.Items))
+		amounts := make([]int64, len(delegation.Items))
+		delegators := make([]string, len(delegation.Items))
+		blocks := make([]string, len(delegation.Items))
+
+		for i, row := range delegation.Items {
+			ids[i] = row.ID
+			timestamps[i] = row.Timestamp
+			amounts[i] = row.Amount
+			delegators[i] = row.NewDelegate.Address
+			blocks[i] = row.Block
+		}
+		rr, err := db.Get(dbURL).ExecContext(ctx, `
+		INSERT INTO delegations
+		(id, timestamp, amount, delegator, block)
+		(SELECT  * FROM UNNEST($1::bigint[], $2::timestamp[], $3::bigint[], $4::varchar[], $5::text[]))
+		ON CONFLICT (id) DO UPDATE SET delegator = EXCLUDED.delegator
+		`, ids, timestamps, amounts, delegators, blocks)
+		if err != nil {
+			log.Printf("[ERROR] Failed to save : %s\n", err)
+			return
+		}
+		cnt, _ := rr.RowsAffected()
+		log.Printf("[INFO] %d delegations saved", cnt)
+	}
 }
